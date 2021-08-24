58ff062bf6b6ad7314c2243b911275bf4c791dfe
MSSangeetha <sangeethasanthosh.m@gmail.com>
2021-08-24 14:55:40 -0500

    Interfaces excercises without studio

--- src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -6,5 +6,6 @@ public class FlavorComparator implements Comparator<Flavor> {
     @Override
     public int compare(Flavor flavor1, Flavor flavor2) {
         return flavor1.getName().compareTo(flavor2.getName());
+//        return flavor1.getAllergens().equals(flavor2.getAllergens());
     }
 }
